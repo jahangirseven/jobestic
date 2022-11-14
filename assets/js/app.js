b1d9bef28d15b94d830a25ede66afe009ae9253f
Jahangir Seven <pregmatch5@gmail.com>
2022-11-14 06:24:45 +0400

    testimonial section added

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,19 @@ const bannerStatsItems = {
 increaseNumber(0,3500,0,bannerStatsItems.jobs);
 increaseNumber(0,1150,1,bannerStatsItems.projects);
 increaseNumber(0,350,10,bannerStatsItems.freelancers);
+
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
